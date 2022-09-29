6f64851aa18ca13918f07bb8753dd215ef66689d
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-09-29 02:12:11 +0000

    [Stop4]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,55 @@
 class BooksController < ApplicationController
+  def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @book.user
+  end
+
   def index
-     @book = Book.new
-     @books = Book.page(params[:page])
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
-    @book = Book.new(books_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
-
-  def show
-    @books = Book.find(params[:id])
+    if @book.save
+      flash[:notice]=""
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]=""
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to book_path
+    if @book.destroy
+      flash[:notice]=""
+      redirect_to books_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
+  devise_for :users
   get "/homes/about" => "homes#about", as: "about"
   resources :books, only: [:index, :create, :show, :edit, :destroy]
+  
   resources :users, only: [:index, :show, :edit, :update]
 
-  devise_for :users
+ 
 
 end
