4f1d7da6f5000d1ad9f4501c5407722f43541c19
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-10-04 09:31:55 +0000

    [Stop7]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = Book.new
-    @books = Book.where(user_id: @user.id)
+    @books = @user.books
+    @newbook = Book.new
   end
 
   def index
     @user = current_user
     @users = User.all
-    @book =Book.new
+    @newbook = Book.new
     @books = Book.all
   end
 
